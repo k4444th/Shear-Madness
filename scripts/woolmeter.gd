fa30f490ce227141271b82dfd504a8d6a52486fa
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-08-24 15:14:36 +0200

    Animate woolmeter

--- scripts/woolmeter.gd
+++ scripts/woolmeter.gd
@@ -8,4 +8,7 @@ func updateWool(wool: int):
 		if int(str(element.name)[4]) <= wool:
 			element.visible = true
 		else:
+			var tween = get_tree().create_tween()
+			tween.tween_property(element, "scale:x", 0, 0.25)
+			await tween.finished
 			element.visible = false
