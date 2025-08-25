38b1d125072fdfa29af584cc719864e0ae912cca
k4444th <95476869+k4444th@users.noreply.github.com>
2025-08-25 13:40:03 +0200

    Basic button functionalities

--- scripts/startmenu.gd
+++ scripts/startmenu.gd
@@ -2,3 +2,12 @@ extends Node
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_settings_button_pressed() -> void:
+	print("Open Settings")
+
+func _on_story_button_pressed() -> void:
+	print("Open Story")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
