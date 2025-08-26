b4a3c98b5b5e13796fa9d957bb2b1d02fe9857fb
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-08-26 13:37:20 +0200

    Startscreen graphics

--- scripts/startmenu.gd
+++ scripts/startmenu.gd
@@ -1,5 +1,12 @@
 extends Node
 
+@onready var camera = $Camera2D
+
+func _process(delta: float) -> void:
+	var zoom = (get_viewport().size.x / 500 + get_viewport().size.y / 300) / 2
+	camera.zoom = Vector2(zoom, zoom)
+	
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
