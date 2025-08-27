58300410286ede7fb83b1dc96255d4e67149a5c9
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-08-27 07:27:28 +0200

    Make startscreen logo prettier

--- scripts/startmenu.gd
+++ scripts/startmenu.gd
@@ -1,11 +1,16 @@
 extends Node
 
 @onready var camera = $Camera2D
+@onready var logo = $CanvasLayer/Logo
 
 func _process(delta: float) -> void:
-	var zoom = (get_viewport().size.x / 500 + get_viewport().size.y / 300) / 2
+	var zoom = (float(get_viewport().size.x) / 500 + float(get_viewport().size.y) / 300) / 2
 	camera.zoom = Vector2(zoom, zoom)
 	
+	logo.scale = Vector2(zoom * 2, zoom * 2)
+	logo.get_children()[0].size.x = (camera.get_viewport().size.x / camera.get_canvas_transform().x[0]) / 2
+	logo.get_children()[1].size.x = (camera.get_viewport().size.x / camera.get_canvas_transform().x[0]) / 2
+	logo.position.y = 30 + zoom * 5
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
